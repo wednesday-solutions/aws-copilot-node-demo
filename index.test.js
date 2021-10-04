a7124082337ba2836566f521b5591aa370a74a33
Mac <mohammed.ali.chherawalla@gmail.com>
2021-10-04 17:24:21 +0530

    add more workflows

--- index.test.js
+++ index.test.js
@@ -1,11 +1,19 @@
-const { app } = require('./index')
+const { app, init } = require('./index')
 const supertest = require ('supertest')
 
 describe("health check API", () => {
     it("Check that health check API returns 200 on /", async () => {
-        expect(1).toBe(1)
         const res = await supertest(app).get('/');
         expect(res.statusCode).toBe(200);
         expect(res.body).toStrictEqual({data: 'Health check api'});
+    })
+})
+describe('init', () => {
+    it("should only invoke the app.listen when the NODE_ENV is not test", () => {
+        process.env.NODE_ENV = "production";
+        const listenSpy = jest.spyOn(app, 'listen').mockReturnValue({ });
+        init();
+        expect(listenSpy).toBeCalled()
+        process.env.NODE_ENV = "test";
     })
 })
